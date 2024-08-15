76416cdc1eeb77001ee73788a9df4e8d0f0a0a06
Blackerbl <162997554+Blackerbl@users.noreply.github.com>
2024-08-15 15:50:37 +0300

    en.js

--- languages/en.js
+++ languages/en.js
@@ -1,255 +1,269 @@
-
 module.exports = {
-    // Play command
-    playDescription: 'Play a song',
-    playErrorNotInVoiceChannel: 'You need to be in a voice channel to use this command!',
-    playSearchingForSong: 'Searching for {song}...',
-    playErrorTitle: 'Error',
-    playSearchingTitle: 'Searching for Song',
-  // Volume command
-  volumeDescription: 'Set the volume',
-  volumeNoSongPlaying: 'There is no song playing right now!',
-  volumeInvalidLevel: 'Volume level must be between 0 and 100.',
-  volumeSetTitle: 'Volume Set',
-  volumeSetDescription: 'Volume set to {volume}%.',  
-  // Time command
-  timeDescription: 'Show the current playback time',
-  timeNoSongPlaying: 'There is no song playing right now!',
-  timeTitle: 'Current Playback Time',
-  timeDescriptionText: 'Current time: `{currentTime}` / `{duration}`',
-    // Stop command
-    stopDescription: 'Stop the music and clear the queue',
-    stopNoSongPlaying: 'There is no song playing right now!',
-    stopTitle: 'Music Stopped',
-    stopDescriptionText: 'The music has been stopped and the queue has been cleared.',
-
-     // Skip command
-  skipDescription: 'Skip the current song',
-  skipNoSongPlaying: 'There is no song playing right now!',
-  skipNoMoreSongs: 'No more songs to skip!',
-  skipTitle: 'Song Skipped',
-  skipDescriptionText: 'Skipped the current song!',
-   // Shuffle command
-   shuffleDescription: 'Shuffle the queue',
-   shuffleNoSongPlaying: 'There is no song playing right now!',
-   shuffleTitle: 'Queue Shuffled',
-   shuffleDescriptionText: 'The queue has been shuffled.',
-    // Seek command
-  seekDescription: 'Seek to a specific time in the song',
-  seekNoSongPlaying: 'There is no song playing right now!',
-  seekInvalidTime: 'The time must be within the duration of the song.',
-  seekTitle: 'Seeked',
-  seekDescriptionText: 'Seeked to {time} in the song.',
-    // Resume command
-    resumeDescription: 'Resume the paused song',
-    resumeNoSongPlaying: 'There is no song playing right now!',
-    resumeAlreadyPlaying: 'The song is already playing!',
-    resumeTitle: 'Song Resumed',
-    resumeDescriptionText: 'Resumed the song!',
-      // Remove command
-  removeDescription: 'Remove a song from the queue',
-  removeNoSongPlaying: 'There is no song playing right now!',
-  removeInvalidIndex: 'Invalid song index.',
-  removeTitle: 'Removed',
-  removeDescriptionText: 'Removed {song} from the queue.',
-   // Queue command
-   queueDescription: 'Show the current queue',
-   queueNoSongPlaying: 'There is no song playing right now!',
-   queueTitle: 'Current Queue',
-   queueFooterText: 'Total duration: {duration}',
-     // Previous command
-  previousDescription: 'Play the previous song',
-  previousNoSongPlaying: 'There is no song playing right now!',
-  previousTitle: 'Playing Previous Song',
-  previousDescriptionText: 'Playing the previous song in the queue.',
-    // Pause command
-    pauseDescription: 'Pause the current song',
-    pauseNoSongPlaying: 'There is no song playing right now!',
-    pauseAlreadyPaused: 'The song is already paused!',
-    pauseTitle: 'Song Paused',
-    pauseDescriptionText: 'Paused the song!',
-     // Now Playing command
-  nowplayingDescription: 'Show the currently playing song',
-  nowplayingNoSong: 'There is no song playing right now!',
-  nowplayingTitle: 'Now Playing',
-    // Loop command
-    loopDescription: 'Set the loop mode',
-    loopNoSong: 'There is no song playing right now!',
-    loopInvalidMode: 'Invalid loop mode. Valid modes are 0 (off), 1 (song), 2 (queue).',
-    loopModeDisabled: 'Loop mode disabled',
-    loopSongEnabled: 'Song loop enabled',
-    loopQueueEnabled: 'Queue loop enabled',
-    loopTitle: 'Loop Mode Set',
-      // Jump command
-  jumpDescription: 'Jump to a specific song in the queue',
-  jumpNoSong: 'There is no song playing right now!',
-  jumpInvalidIndex: 'Invalid song index.',
-  jumpTitle: 'Jumped',
-  jumpSuccess: 'Jumped to song number {index}.',
-  findDescription: 'Search for a song',
-  findNoVoiceChannel: 'You need to be in a voice channel to use this command.',
-  findNoResults: 'No songs found for your query.',
-  findTitle: 'Search Results',
-  findPrompt: 'Select a song to add to the queue:',
-  findSongSelected: 'Selected song **{title}** will be played!',
-  //filters.js
-  filters: {
-    description: 'Show available audio filters and apply one by clicking a button',
-    errorTitle: 'Error',
-    noSongPlaying: 'There is no song playing right now!',
-    availableFiltersTitle: 'Available Filters',
-    clickButtonToApply: 'Click a button to apply a filter',
-    clearFiltersLabel: 'Clear Filters',
-    notYourButton: 'You cannot use this button!',
-    filtersCleared: 'All filters have been cleared.',
-    appliedFilter: 'Applied filter',
-    timeout: 'Filter selection timed out.'
-  },
-  clear: {
-    description: 'Clear the queue',
-    errorTitle: 'Error',
-    noSongPlaying: 'There is no song playing right now!',
-    queueClearedTitle: 'Queue Cleared',
-    queueClearedDescription: 'The queue has been cleared.'
-  },
-  autoplay: {
-    description: 'Toggle autoplay',
-    errorTitle: 'Error',
-    noSongPlaying: 'There is no song playing right now!',
-    autoplayToggledTitle: 'Autoplay Toggled',
-    autoplayStatus: 'Autoplay is now',
-    enabled: 'enabled',
-    disabled: 'disabled'
-  },
-  addsong: {
-    description: 'Add a song to a playlist',
-    playlistDesc: 'The playlist name',
-    songDesc: 'The song URL or name',
-    errorAccess: 'An error occurred while accessing the playlist.',
-    errorNotFound: 'Playlist not found or you are not the owner.',
-    errorAdd: 'An error occurred while adding the song.',
-    successTitle: 'Song Added',
-    addedSong: 'Added song',
-    toPlaylist: 'to playlist'
-  },
-  createplaylist: {
-    description: 'Create a new playlist',
-    nameDesc: 'The name of the playlist',
-    visibilityDesc: 'Public or Private',
-    error: 'An error occurred while creating the playlist.',
-    successTitle: 'Playlist Created',
-    successMessage: 'Playlist has been created successfully.',
-    visibilityLabel: 'Visibility',
-    public: 'Public',
-    private: 'Private'
-  },
-  deleteplaylist: {
-    description: 'Delete a playlist',
-    playlistDesc: 'The playlist name to delete',
-    error: 'An error occurred while processing the request.',
-    notFound: 'Playlist not found.',
-    noPermission: 'You do not have permission to delete this playlist.',
-    successTitle: 'Playlist Deleted',
-    successMessage: 'Playlist has been deleted'
-  },
-  playplaylist: {
-    description: 'Play a playlist',
-    playlistDesc: 'The playlist name',
-    notInVoiceChannel: 'You need to be in a voice channel to use this command.',
-    error: 'An error occurred while accessing the playlist.',
-    notFound: 'Playlist not found.',
-    noPermission: 'This playlist is private and you are not the owner.',
-    emptyPlaylist: 'The playlist is empty.',
-    successTitle: 'Playlist Started',
-    successMessage: 'Started playing playlist',
-    failedToPlay: 'Failed to play the playlist.',
-  },
-  removesong: {
-    description: 'Remove a song from a playlist',
-    playlistDesc: 'The playlist name',
-    songDesc: 'The song URL or name',
-    error: 'An error occurred while accessing the playlist.',
-    notFound: 'Playlist not found or you are not the owner.',
-    removeError: 'An error occurred while removing the song.',
-    successTitle: 'Song Removed',
-    successMessage: 'Removed',
-    fromPlaylist: 'from playlist',
+    // Oynat komutu
+    playDescription: 'Bir şarkı çal',
+    playErrorNotInVoiceChannel: 'Bu komutu kullanmak için bir ses kanalında olmalısınız!',
+    playSearchingForSong: '{song} için arama yapılıyor...',
+    playErrorTitle: 'Hata',
+    playSearchingTitle: 'Şarkı Aranıyor',
+  
+    // Ses seviyesi komutu
+    volumeDescription: 'Ses seviyesini ayarla',
+    volumeNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    volumeInvalidLevel: 'Ses seviyesi 0 ile 100 arasında olmalıdır.',
+    volumeSetTitle: 'Ses Seviyesi Ayarlandı',
+    volumeSetDescription: 'Ses seviyesi {volume}%'ye ayarlandı.',
+  
+    // Zaman komutu
+    timeDescription: 'Geçerli oynatma zamanını göster',
+    timeNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    timeTitle: 'Geçerli Oynatma Zamanı',
+    timeDescriptionText: 'Geçerli zaman: {currentTime}` / `{duration}',
+  
+    // Durdur komutu
+    stopDescription: 'Müziği durdur ve kuyruğu temizle',
+    stopNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    stopTitle: 'Müzik Durduruldu',
+    stopDescriptionText: 'Müzik durduruldu ve kuyruk temizlendi.',
+  
+    // Atla komutu
+    skipDescription: 'Geçerli şarkıyı atla',
+    skipNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    skipNoMoreSongs: 'Atlayacak başka şarkı yok!',
+    skipTitle: 'Şarkı Atlandı',
+    skipDescriptionText: 'Geçerli şarkı atlandı!',
+  
+    // Karıştır komutu
+    shuffleDescription: 'Kuyruğu karıştır',
+    shuffleNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    shuffleTitle: 'Kuyruk Karıştırıldı',
+    shuffleDescriptionText: 'Kuyruk karıştırıldı.',
+  
+    // İleri sar komutu
+    seekDescription: 'Şarkıda belirli bir zamana git',
+    seekNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    seekInvalidTime: 'Zaman, şarkının süresi içinde olmalıdır.',
+    seekTitle: 'İleri Sarıldı',
+    seekDescriptionText: '{time} zamanına gidildi.',
+  
+    // Devam et komutu
+    resumeDescription: 'Duraklatılan şarkıyı devam ettir',
+    resumeNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    resumeAlreadyPlaying: 'Şarkı zaten çalıyor!',
+    resumeTitle: 'Şarkı Devam Ettirildi',
+    resumeDescriptionText: 'Şarkı devam ettirildi!',
+  
+    // Kaldır komutu
+    removeDescription: 'Kuyruktan bir şarkıyı kaldır',
+    removeNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    removeInvalidIndex: 'Geçersiz şarkı indeksi.',
+    removeTitle: 'Kaldırıldı',
+    removeDescriptionText: '{song} kuyruktan kaldırıldı.',
+  
+    // Kuyruk komutu
+    queueDescription: 'Mevcut kuyruğu göster',
+    queueNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    queueTitle: 'Mevcut Kuyruk',
+    queueFooterText: 'Toplam süre: {duration}',
+  
+    // Önceki komut
+    previousDescription: 'Önceki şarkıyı çal',
+    previousNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    previousTitle: 'Önceki Şarkı Çalınıyor',
+    previousDescriptionText: Kuyruktaki önceki şarkı çalınıyor.',
+  
+    // Duraklat komutu
+    pauseDescription: 'Geçerli şarkıyı duraklat',
+    pauseNoSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+    pauseAlreadyPaused: 'Şarkı zaten duraklatıldı!',
+    pauseTitle: 'Şarkı Duraklatıldı',
+    pauseDescriptionText: 'Şarkı duraklatıldı!',
+  
+    // Şu anda çalan komut
+    nowplayingDescription: 'Şu anda çalan şarkıyı göster',
+    nowplayingNoSong: 'Şu anda çalınan bir şarkı yok!',
+    nowplayingTitle: 'Şu Anda Çalan',
+  
+    // Döngü komutu
+    loopDescription: 'Döngü modunu ayarla',
+    loopNoSong: 'Şu anda çalınan bir şarkı yok!',
+    loopInvalidMode: 'Geçersiz döngü modu. Geçerli modlar 0 (off), 1 (song), 2 (queue).',
+    loopModeDisabled: 'Döngü modu devre dışı bırakıldı',
+    loopSongEnabled: 'Şarkı döngüsü etkinleştirildi',
+    loopQueueEnabled: 'Kuyruk döngüsü etkinleştirildi',
+    loopTitle: 'Döngü Modu Ayarlandı',
+  
+    // Atlama komutu
+    jumpDescription: 'Kuyruktaki belirli bir şarkıya atla',
+    jumpNoSong: 'Şu anda çalınan bir şarkı yok!',
+    jumpInvalidIndex: 'Geçersiz şarkı indeksi.',
+    jumpTitle: 'Atlandı',
+    jumpSuccess: '{index} numaralı şarkıya atlandı.',
+  
+    // Şarkı bul komutu
+    findDescription: 'Bir şarkı ara',
+    findNoVoiceChannel: 'Bu komutu kullanmak için bir ses kanalında olmalısınız.',
+    findNoResults: 'Sorgunuz için şarkı bulunamadı.',
+    findTitle: 'Arama Sonuçları',
+    findPrompt: 'Kuyruğa eklemek için bir şarkı seçin:',
+    findSongSelected: '**{title}** şarkısı seçildi ve çalınacak!',
+  
+    // filtreler.js
+    filters: {
+        description: 'Mevcut ses filtrelerini göster ve birine butona tıklayarak uygula',
+        errorTitle: 'Hata',
+        noSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+        availableFiltersTitle: 'Mevcut Filtreler',
+        clickButtonToApply: 'Bir filtre uygulamak için bir butona tıklayın',
+        clearFiltersLabel: 'Filtreleri Temizle',
+        notYourButton: 'Bu butonu kullanamazsınız!',
+        filtersCleared: 'Tüm filtreler temizlendi.',
+        appliedFilter: 'Uygulanan filtre',
+        timeout: 'Filtre seçimi zaman aşımına uğradı.'
+    },
+  
+    clear: {
+        description: 'Kuyruğu temizle',
+        errorTitle: 'Hata',
+        noSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+        queueClearedTitle: 'Kuyruk Temizlendi',
+        queueClearedDescription: 'Kuyruk temizlendi.'
+    },
+  
+    autoplay: {
+        description: 'Otomatik çalmayı aç/kapat',
+        errorTitle: 'Hata',
+        noSongPlaying: 'Şu anda çalınan bir şarkı yok!',
+        autoplayToggledTitle: 'Otomatik Çalma Değiştirildi',
+        autoplayStatus: 'Otomatik çalma şimdi',
+        enabled: 'etkin',
+        disabled: 'devre dışı'
+    },
+  
+    addsong: {
+        description: 'Bir şarkıyı çalma listesine ekle',
+        playlistDesc: 'Çalma listesi adı',
+        songDesc: 'Şarkı URL’si veya adı',
+        errorAccess: 'Çalma listesine erişirken bir hata oluştu.',
+        errorNotFound: 'Çalma listesi bulunamadı veya sahibi değilsiniz.',
+        errorAdd: 'Şarkıyı eklerken bir hata oluştu.',
+        successTitle: 'Şarkı Eklendi',
+        addedSong: 'Şarkı eklendi',
+        toPlaylist: 'çalma listesine'
+    },
+  
+    createplaylist: {
+        description: 'Yeni bir çalma listesi oluştur',
+        nameDesc: 'Çalma listesinin adı',
+        visibilityDesc: 'Açık veya Özel',
+        error: 'Çalma listesi oluştururken bir hata oluştu.',
+        successTitle: 'Çalma Listesi Oluşturuldu',
+        successMessage: 'Çalma listesi başarıyla oluşturuldu.',
+        visibilityLabel: 'Görünürlük',
+        public: 'Açık',
+        private: 'Özel'
+    },
+  
+    deleteplaylist: {
+        description: 'Bir çalma listesini sil',
+        playlistDesc: 'Silinecek çalma listesi adı',
+        error: 'İsteği işleme alırken bir hata oluştu.',
+        notFound: 'Çalma listesi bulunamadı.',
+        successTitle: 'Çalma Listesi Silindi',
+        successMessage: 'Çalma listesi başarıyla silindi.'
+    },
+  
+    playplaylist: {
+        description: 'Bir çalma listesini çal',
+        playlistDesc: 'Çalınacak çalma listesi adı',
+        error: 'Çalma listesi çalarken bir hata oluştu.',
+        notFound: 'Çalma listesi bulunamadı.',
+        successTitle: 'Çalma Listesi Çalınıyor',
+        successMessage: 'Çalma listesi çalınıyor: {playlistName}'
+    }
+removesong: {
+    description: 'Bir şarkıyı çalma listesinden kaldır',
+    playlistDesc: 'Çalma listesi adı',
+    songDesc: 'Şarkı URL’si veya adı',
+    error: 'Çalma listesine erişirken bir hata oluştu.',
+    notFound: 'Çalma listesi bulunamadı veya sahibi değilsiniz.',
+    removeError: 'Şarkıyı kaldırırken bir hata oluştu.',
+    successTitle: 'Şarkı Kaldırıldı',
+    successMessage: 'Kaldırıldı',
+    fromPlaylist: 'çalma listesinden',
   },
   showallplaylists: {
-    description: 'Show all playlists',
-    error: 'An error occurred while fetching playlists.',
-    noPlaylists: 'No playlists found.',
-    title: 'All Playlists',
-    page: 'Page',
-    songs: 'songs',
-    by: 'By',
-    visibility: 'Visibility',
-    public: 'Public',
-    private: 'Private',
+    description: 'Tüm çalma listelerini göster',
+    error: 'Çalma listelerini getirirken bir hata oluştu.',
+    noPlaylists: 'Hiç çalma listesi bulunamadı.',
+    title: 'Tüm Çalma Listeleri',
+    page: 'Sayfa',
+    songs: 'şarkılar',
+    by: 'Tarafından',
+    visibility: 'Görünürlük',
+    public: 'Açık',
+    private: 'Özel',
   },
   showplaylist: {
-    description: 'Show your playlists',
-    error: 'An error occurred while fetching your playlists.',
-    noPlaylists: 'You have no playlists.',
-    title: 'Your Playlists',
-    page: 'Page',
-    songs: 'songs',
-    visibility: 'Visibility',
-    public: 'Public',
-    private: 'Private',
+    description: 'Çalma listelerinizi göster',
+    error: 'Çalma listelerinizi getirirken bir hata oluştu.',
+    noPlaylists: 'Hiç çalma listeniz yok.',
+    title: 'Sizin Çalma Listeleriniz',
+    page: 'Sayfa',
+    songs: 'şarkılar',
+    visibility: 'Görünürlük',
+    public: 'Açık',
+    private: 'Özel',
   },
   playlist: {
-    errorNotInVoiceChannel: 'You need to be in a voice channel to use this command.',
-    playlistAdded: 'The playlist has been added to the queue!',
-    errorAddingPlaylist: 'An error occurred while adding the playlist to the queue.',
-    titleError: 'Error',
-    titlePlaylistAdded: 'Playlist Added',
-    pdescription: 'play a playlist',
+    errorNotInVoiceChannel: 'Bu komutu kullanmak için bir ses kanalında olmalısınız.',
+    playlistAdded: 'Çalma listesi kuyruğa eklendi!',
+    errorAddingPlaylist: 'Çalma listesini kuyruğa eklerken bir hata oluştu.',
+    titleError: 'Hata',
+    titlePlaylistAdded: 'Çalma Listesi Eklendi',
+    pdescription: 'bir çalma listesi çal',
   },
   lyrics: {
-    description: 'Get lyrics of a song',
-    songDescription: 'The song to get lyrics for',
-    noLyricsFound: 'No lyrics found for that song.',
+    description: 'Bir şarkının sözlerini al',
+    songDescription: 'Sözlerini almak istediğiniz şarkı',
+    noLyricsFound: 'Bu şarkı için söz bulunamadı.',
   },
   showplaylistsongs: {
-    description: 'Show all songs in a playlist.',
-    playlistDesc: 'Name of the playlist.',
-    errorAccess: 'An error occurred while accessing the playlist.',
-    errorNotFound: 'Playlist not found.',
-    title: 'Songs in Playlist',
-    noSongs: 'No songs in the playlist.',
+    description: 'Bir çalma listesindeki tüm şarkıları göster.',
+    playlistDesc: 'Çalma listesinin adı.',
+    errorAccess: 'Çalma listesine erişirken bir hata oluştu.',
+    errorNotFound: 'Çalma listesi bulunamadı.',
+    title: 'Çalma Listesindeki Şarkılar',
+    noSongs: 'Çalma listesinde hiç şarkı yok.',
   },
   playSong: {
-    authorName: 'Playing..',
-    url: 'https://discord.gg/xQF9f9yUEM',
-    song: 'Song',
-    duration: 'Duration',
-    source: 'Source'
+    authorName: 'Çalınıyor..',
+    url: 'https://discord.gg/XGwTFHhw7z',
+    song: 'Şarkı',
+    duration: 'Süre',
+    source: 'Kaynak'
   },
   addSong: {
-    authorName: 'Added to Queue',
-    url: 'https://discord.gg/xQF9f9yUEM',
-    addedToQueue: 'added to the queue'
+    authorName: 'Kuyruğa Eklendi',
+    url: 'https://discord.gg/XGwTFHhw7z',
+    addedToQueue: 'kuyuğa eklendi'
   },
   error: {
-    authorName: 'Alert!',
-    url: 'https://discord.gg/xQF9f9yUEM',
-    errorMessage: 'An error encountered'
+    authorName: 'Uyarı!',
+    url: 'https://discord.gg/XGwTFHhw7z',
+    errorMessage: 'Bir hata meydana geldi'
   },
   finish: {
-    authorName: 'Queue Ended',
-    url: 'https://discord.gg/xQF9f9yUEM',
-    description: 'The queue has finished!'
+    authorName: 'Kuyruk Bitti',
+    url: 'https://discord.gg/XGwTFHhw7z',
+    description: 'Kuyruk tamamlandı!'
   },
   disconnect: {
-    authorName: 'Disconnected',
-    url: 'https://discord.gg/xQF9f9yUEM',
-    description: 'Disconnected from the voice channel'
+    authorName: 'Bağlantı Kesildi',
+    url: 'https://discord.gg/XGwTFHhw7z',
+    description: 'Ses kanalından bağlantı kesildi'
   },
   empty: {
-    authorName: 'Channel Empty',
-    url: 'https://discord.gg/xQF9f9yUEM',
-    description: 'The voice channel is empty, leaving the channel.'
+    authorName: 'Kanal Boş',
+    url: 'https://discord.gg/XGwTFHhw7z',
+    description: 'Ses kanalı boş, kanaldan çıkılıyor.'
   }
-  };
-  
